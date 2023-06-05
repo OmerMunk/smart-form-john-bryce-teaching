bc6a42cf7a08880a9e74bcf7a317c6ba74c77932
omer.munk <omermunk95@gmail.com>
2023-06-05 17:18:02 +0100

    added navigation to app

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import Box from "./components/Box/Box";
 import Button from "./components/Button/Button";
 
 import {Routes, Route} from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
 
     return (
         <>
-            <button>Home</button>
-            <button>About us</button>
-            <button>Contact us</button>
+            <Navigation/>
             <Routes>
                 <Route path={'/home'} element={<h1>This is home</h1>}/>
                 <Route path={'/about'} element={<h1>This is about</h1>}/>
