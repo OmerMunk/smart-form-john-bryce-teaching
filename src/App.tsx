ed4e567ac8ed45d2ef33454b44169734b8c8b7b9
omer.munk <omermunk95@gmail.com>
2023-06-05 16:37:54 +0100

    finish last task

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ function App() {
     const [amount, setAmount] = React.useState<number>(0);
     const [myString, setMyString] = useState<string>('a');
 
+    const [name, setName] = useState<string>('')
+    const [nameDisplay, setNameDisplay] = useState<string>('Box1')
+
     const increment = () => {
         setAmount(prevState => prevState + 1);
     };
@@ -28,20 +31,27 @@ function App() {
             <button>About us</button>
             <button>Contact us</button>
             <Box color={'red'} width={'100px'} height={'100px'}>
-                <h1>Box 1</h1>
+                <h1>{nameDisplay}</h1>
             </Box>
-
             <Box color={'blue'} width={'50px'} height={'50px'}/>
-
             <Box title={`Amount: ${amount.toString()}`} color={'white'}/>
-
             <Box title={`String: ${myString}`} color={'white'}/>
-
             <Button buttonType={"primary"} onClick={increment} text={'Click Me!'}/>
             <Button buttonType={"secondary"} onClick={decrement} text={'Click Me2!'}/>
-
             <Button buttonType={'primary'} onClick={addA} text={'Add "a"'}/>
             <Button buttonType={'secondary'} onClick={removeA} text={'remove "a"'}/>
+            <br/>
+            <input
+                type='text'
+                onChange={(event)=>{
+                    setName(event.target.value)
+                }}
+            />
+            <button
+                onClick={()=>{
+                    setNameDisplay(name)
+                }}
+            >Update</button>
         </>
     );
 }
